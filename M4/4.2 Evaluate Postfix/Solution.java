ac378e731f4390e554d567897b78b586c6641c0c
N V SIVA CHANIKYA VARMA <chanikya012@gmail.com>
2018-10-06 10:10:06 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- M4/4.2 Evaluate Postfix/Solution.java
+++ M4/4.2 Evaluate Postfix/Solution.java
@@ -45,10 +45,47 @@ import java.io.*;
 }*/
 class Solution
 {
-   public static void main(String[] args)
+   public static void main(String[] args)throws Exception
 {
-   System.out.println("hii");
+                String exp;
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		exp=br.readLine();
+		Solution p=new Solution();
+		System.out.println("hii");//p.evaluate(exp)
+
 }
+int  evaluate(String exp) {
+		String str[] = exp.split(" ");
+		int n = 0, l = str.length;
+		Stack<Integer> stack = new Stack<>();
+		for (int i = 0; i < l; i++) {
+			try {
+				n = Integer.parseInt(str[i]);
+				stack.push(n);
+			} catch (NumberFormatException e) {
+				int v1 = stack.pop();
+				int v2 = stack.pop();
+				switch (str[i].charAt(0)) {
+				case '+':
+					n = v1 + v2;
+					break;
+				case '-':
+					n = v2 - v1;
+					break;
+				case '*':
+					n = v1 * v2;
+					break;
+				case '/':
+					n = v2 / v1;
+					break;
+
+				}
+				stack.push(n);
+			}
+
+		}
+		return (stack.pop());
+	}
 }
 
 
