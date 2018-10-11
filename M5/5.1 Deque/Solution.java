e933bef1fb036f3886894b334391fe8a1e644992
N V SIVA CHANIKYA VARMA <chanikya012@gmail.com>
2018-10-11 14:18:00 +0530

    "check_style_score -> 3 of 4 passed. style score: 0/1 "

--- M5/5.1 Deque/Solution.java
+++ M5/5.1 Deque/Solution.java
@@ -1,45 +1,83 @@
-import java.util.*; 
-  
-public class Solution 
-{ 
-    public static void main(String args[]) 
-    { 
-        // Creating object of class linked list 
-        LinkedList<String> object = new LinkedList<String>(); 
-  
-        // Adding elements to the linked list 
-        object.add("A"); 
-        object.add("B"); 
-        object.addLast("C"); 
-        object.addFirst("D"); 
-        object.add(2, "E"); 
-        object.add("F"); 
-        object.add("G"); 
-        System.out.println("Linked list : " + object); 
-  
-        // Removing elements from the linked list 
-        object.remove("B"); 
-        object.remove(3); 
-        object.removeFirst(); 
-        object.removeLast(); 
-        System.out.println("Linked list after deletion: " + object); 
-  
-        // Finding elements in the linked list 
-        boolean status = object.contains("E"); 
-  
-        if(status) 
-            System.out.println("List contains the element 'E' "); 
-        else
-            System.out.println("List doesn't contain the element 'E'"); 
-  
-        // Number of elements in the linked list 
-        int size = object.size(); 
-        System.out.println("Size of linked list = " + size); 
-  
-        // Get and set elements from linked list 
-        Object element = object.get(2); 
-        System.out.println("Element returned by get() : " + element); 
-        object.set(2, "Y"); 
-        System.out.println("Linked list after change : " + object); 
-    } 
-} 
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Dequeue1 {
+	LinkedList<Integer> ll = new LinkedList<Integer>();
+	public void pushRight(int item) {
+		ll.add(item);
+	}
+	
+	public void popLeft() {
+		ll.removeFirst();
+	}
+
+	public void pushLeft(int item) {
+		ll.addFirst(item);
+	}
+
+	public void popRight() {
+		ll.removeLast();
+	}
+
+	public String print() {
+
+		if (ll.isEmpty()) {
+			//System.out.println("Hi");
+			return "[]";
+		}
+		String st = "[" + ll.get(0);
+		for (int i = 1; i < ll.size(); i++) {
+			st = st + ", " + ll.get(i);
+		}
+		st = st + "]";
+		// System.out.println();
+		return st;
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n, i, data;
+		String s;
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+		Dequeue1 d = new Dequeue1();
+
+		while (scan.hasNext()) {
+			s = scan.nextLine();
+			//System.out.println(s);
+			String[] s1 = s.split(" ");
+			//System.out.println(s1[0]);
+			switch (s1[0]) {
+			case "isEmpty":
+				System.out.println(d.ll.isEmpty());
+				break;
+			case "size":
+				System.out.println(d.ll.size());
+				break;
+			case "pushLeft":
+				data = Integer.parseInt(s1[1]);
+				d.pushLeft(data);
+				System.out.println(d.ll);
+				break;
+			case "pushRight":
+				data = Integer.parseInt(s1[1]);
+				d.pushRight(data);
+				System.out.println(d.ll);
+				break;
+			case "popLeft":
+				d.popLeft();
+				System.out.println(d.print());
+				break;
+			case "popRight":
+				d.popRight();
+				System.out.println(d.print());
+				break;
+			}
+		}
+		
+	}
+
+}
