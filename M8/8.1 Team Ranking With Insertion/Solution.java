500defac4d7263e0c4d5e3017429be175c5305ca
N V SIVA CHANIKYA VARMA <chanikya012@gmail.com>
2018-10-23 14:27:07 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- M8/8.1 Team Ranking With Insertion/Solution.java
+++ M8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,10 +1,10 @@
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+//import java.util.ArrayList;
+import java.util.*;
+/*import java.util.Collections;
 import java.util.Comparator;
 import java.util.Scanner;
-
+*/
 class Teams implements Comparable<Teams> {
 	String Tname;
 	int wins;
